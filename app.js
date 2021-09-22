b362d681fd4b017ba96ce3714cdc823aaea3e68f
ZibiH <zbigniew.holak@gmail.com>
2021-09-22 17:40:26 +0200

    Handle the default video list request

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const https = require('https');
 const express = require('express');
+const path = require('path');
 
 const setHeaders = require('./middleware/headers');
 const fetchYt = require('./controllers/youtube');
 const fetchVim = require('./controllers/vimeo');
+const getDefaultVideoBase = require('./controllers/defaultBase');
 
 const app = express();
 
 app.use(express.json());
 app.use(setHeaders);
 
+app.use('/default', getDefaultVideoBase);
 app.use('/youtube/:id', fetchYt);
 app.use('/vimeo/:id', fetchVim);
 
